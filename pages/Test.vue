23cddb50294b28badf1d654fd9fc74554461c086
vasilev <d5881857@gmail.com>
2022-05-21 13:13:06 +0300

    switch test and index

--- pages/Test.vue
+++ pages/Test.vue
@@ -1,115 +1,73 @@
 <template>
-  <div class="Test">
-    <!-- <div class="save-btn">
-      <button @click="showModal = true">Фото</button>
-      <ModalWindow v-show="showModal" @close-modal="showModal = false" />
-      <selectComponent />
-    </div> -->
-    <filters
-      :view-format="'table'"
-      :data="dataAboutDronoport"
-      :itemsOnPage="4"
-      :placeHolder="placeHolder"
-      :rightSideView="[
-        { name: 'Options', img: 'ico/settings.svg' },
-        { name: 'Filters', img: 'ico/filter.svg' },
-      ]"
-    >
-      <template
-        #default="{
-          viewFormat,
-          showRightSide,
-          typeViewOfRightSide,
-          switchTypeOfView,
-          tableDescription,
-          tableTitle,
-          itemsOnPage,
-          lenghtSearchedData,
-          onChangePage,
-          UpdatedPageNumber,
-          pageNumber,
-        }"
-      >
-        <div class="left-main-content">
-          <tableView
-            v-if="viewFormat === 'table'"
-            :tableDescription="tableDescription"
-            :tableTitle="tableTitle"
-            :pageNumber="pageNumber"
-            :itemsOnPage="itemsOnPage"
-          >
-            <pagination
-              :itemsOnPage="itemsOnPage"
-              :countOfallDronoport="lenghtSearchedData"
-              @UpdatedPageSlice="onChangePage"
-              @UpdatedPageNumber="UpdatedPageNumber"
-            />
-          </tableView>
-          <div v-else-if="viewFormat === 'map'">
-            <Map></Map>
-          </div>
-        </div>
-        <div class="right-main-content" v-if="showRightSide">
-          <rightSide
-            class="option"
-            style="background-color: #fff display: flex; flex-wrap: wrap; gap: 10px"
-            @changeViewFormat="switchTypeOfView"
-          >
-            <div class="buttons" v-if="typeViewOfRightSide === 'Options'">
-              <button @click="switchTypeOfView('map')">Карта</button>
-              <button @click="switchTypeOfView('table')">Таблица</button>
-            </div>
-            <div class="filter" v-else-if="typeViewOfRightSide === 'Filters'">
-              filters
-            </div>
-          </rightSide>
-        </div>
-      </template>
-    </filters>
+  <div class="main-area" id="main-area">
+    <div class="main-area__content" id="main-area-content">
+      <div class="list-of-place">
+        <Table
+          :tableTitle="tableTitle"
+          :tableDescription="tableDataSearched"
+        ></Table>
+      </div>
+    </div>
+    <FilterFD />
   </div>
 </template>
 
 <script>
-import ModalWindow from "../components/ModalWindow.vue";
-import selectComponent from "../components/selectComponent.vue";
-import filters from "../components/filters";
-import rightSide from "../components/right-side.vue";
-import tableView from "../components/table-view.vue";
-import Map from "../components/Map.vue";
-import pagination from "../components/pagination.vue";
+import Table from "../components/Table/Table.vue";
+import FilterFD from "../components/FilterFD.vue";
 
-import { mapGetters } from "vuex";
 export default {
-  layout: "map",
-  components: {
-    ModalWindow,
-    selectComponent,
-    filters,
-    tableView,
-    Map,
-    pagination,
-    rightSide,
-  },
   data() {
     return {
-      placeHolder: "Введите адрес",
-      showModal: false,
+      tableData: [],
     };
   },
-  computed: {
-    ...mapGetters("dronoports", {
-      dataAboutDronoport: "getAllDronoport",
-    }),
+  components: {
+    Table,
+    FilterFD,
   },
-  methods: {},
+  props: {
+    querySearch: {
+      type: String,
+    },
+  },
+  created() {
+    this.tableData = this.$store.getters["dronoports/getAllDronoport"];
+  },
+  computed: {
+    tableTitle() {
+      return this.tableData[0];
+    },
+    tableDataSearched() {
+      let querySearch = this.$store.getters["search/getStoreQuery"]
+        .trim()
+        .toLowerCase()
+        .replace(/[\s.,\s]/g, "");
+      let data = this.tableData[1];
+      return data.filter((elem) => {
+        return elem.address
+          .toLowerCase()
+          .replace(/[\s.,\s]/g, "")
+          .includes(querySearch, 0);
+      });
+    },
+  },
+  methods: {
+    inputValue(value) {
+      this.querySearch = value;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.right-main-content {
-  flex: 0 0 35%;
-}
-.left-main-content {
-  flex: 1 1 auto;
+.main-area {
+  display: flex;
+  gap: 1rem;
+  transition: all 0.5s ease;
+  &__content {
+    width: 100%;
+    transition: all 0.5s ease;
+  }
 }
 </style>
